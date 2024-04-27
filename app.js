4cae6bf20e065420823393e17adbf027f36def97
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-27 23:16:47 +0200

    Use user Routes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
+const userRoutes = require('./routes/User');
 
 const app = express();
 
+// Middleware
+app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/', userRoutes);
 
 module.exports = app;
