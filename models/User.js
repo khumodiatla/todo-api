32542a084643b3ec1980a04a63dc8efa655e0df4
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-27 23:14:09 +0200

    Add user schema

--- models/User.js
+++ models/User.js
@@ -6,23 +6,23 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
       },
-      email: {
-        type: String,
-        required: true,
-        unique: true
-      },
-      firstname: {
-        type: String,
-        require: true
-      },
-      lastname: {
-        type: String,
-        require: true
-      },
-      password: {
-        type: String,
-        required: true
-      }
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    firstname: {
+      type: String,
+      require: true
+    },
+    lastname: {
+      type: String,
+      require: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
     }, {
         timestamps: true
     }
