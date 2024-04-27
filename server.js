65c63901b427b9a10159da758b8a1f9ecf58b8a5
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-27 23:16:15 +0200

    Refactor

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const app = require('./app');
-const dotenv = require('dotenv').config();
+const Database = require('./database');
+
+const database = new Database();
 
 const startServer =  async () => {
     try {
+        await database.connect();
+
         const PORT =  process.env.PORT || 8001;
         const server = app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         })
+
+        process.on('SIGINT', async () => {
+            console.log('Server shutting down...');
+            await database.disconnect();
+
+            server.close(() => {
+                console.log('Server successfully shut down');
+                process.exit(0);
+            })
+        });
+
     } catch (error) {
         console.error('Error while starting server: ', error);
+        await database.disconnect();
     }
 };
 
