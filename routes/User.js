c3ea4178a4e0f008e22aed779a971392482268d3
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-28 21:33:00 +0200

    Add sigin route

--- routes/User.js
+++ routes/User.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/User');
 const userController = new UserController();
 
 router.post('/users/signup', userController.SignUp);
+router.post('/users/signin', userController.signIn);
 
 module.exports = router;
