607660ed6a2d11ccfc6a9b993f252b2e545b8495
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-28 21:32:45 +0200

    Add signin logic

--- controllers/User.js
+++ controllers/User.js
@@ -1,5 +1,7 @@
+const User = require('../models/User');
 const UserService = require('../services/User');
-const hashPassword = require('../utils/passwordUtils');
+const { hashPassword, verifyPassword } = require('../utils/passwordUtils');
+const { generateToken } = require('../utils/tokenUtils');
 
 const userService = new UserService();
 
@@ -37,6 +39,41 @@ class UserController{
         } catch (error) {
             console.error('Error while User signing up: ', error);
             return res.status(500).json({ error: 'Internal Server error'});
+        }
+    }
+
+    async signIn(req, res) {
+        try {
+            const { username, password } = req.body;
+    
+            if(!username || !password){
+                console.error('All the fields are reqiured');
+                return res.status(400).json({ error: 'All the fields are required'});
+            }
+    
+            const user = await userService.getUserByUsername(username);
+            if(!user){
+                console.error('User with given username not found');
+                return res.status(404).json({error: 'User not found'});
+            }
+            
+            // Verify password
+            const isPasswordMatch = verifyPassword(password, user.password);
+            if(!isPasswordMatch){
+                console.error('User password does not match the hashed password');
+                return res.status(401).json({ error: 'Invalid password'});
+            }
+            // Generate token
+            const token = await generateToken(user.id);
+
+            // Remove password from the user response
+            const { password: hashedPassword, ...userOutput} = user.toObject();
+            userOutput.token = token;
+    
+            return res.json(userOutput);
+        } catch (error) {
+            console.error('Error while User signing in:', error);
+            return res.status(500).json({ error: 'Internal server error'});
         }
     }
 }
