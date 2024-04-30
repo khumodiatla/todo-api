03797cab802246085728b585a9670020c0a64a4b
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-04-30 18:20:12 +0200

    Add Todo Controller - Create a todo

---
+++ controllers/Todo.js
@@ -0,0 +1,36 @@
+const TodoService = require('../services/Todo');
+
+const todoService = new TodoService();
+
+class TodoController {
+    async createTodo(req, res){
+        try {
+            const { title, description, completed, dueDate, priority } =  req.body;
+    
+            //  Check if all fields are the
+            if(!title || !description || !completed || !dueDate || !priority){
+                console.error('All the fields are required');
+                return res.status(400).json({error: 'All the fields are required'});
+            }
+    
+            const userId =  req.userId;
+            const todoInput = {
+                userId,
+                title,
+                description,
+                completed,
+                dueDate,
+                priority
+            }
+    
+            const todo = await todoService.createTodo(todoInput)
+
+            return res.status(201).json(todo);
+        } catch (error) {
+            console.error('Error while creating a todo: ', error);
+            return res.status(500).json({ error: 'Internal Server error'});
+        }
+    }
+}
+
+module.exports = TodoController;
