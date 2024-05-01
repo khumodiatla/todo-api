7650865d205f26f6321488bd556d3e5bee5d74ea
Khumodiatla Moatlhodi <khumomoatlhodi@gmail.com>
2024-05-01 15:58:13 +0200

    Add getTodoById route

--- routes/Todo.js
+++ routes/Todo.js
@@ -5,5 +5,6 @@ const authenticateUser =  require('../middlewares/authenticateUser');
 const todoController = new TodoController();
 
 router.post('/todos', authenticateUser, todoController.createTodo);
+router.get('/todos/:todoId', todoController.getTodoById); // Add auth
 
 module.exports = router;
